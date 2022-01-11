a19b0d4d341b5ab5e16eb8a9689c23b28e968bf8
Ian Christiansen <ianc.songdog@gmail.com>
2022-01-11 11:36:12 -0800

    file structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>To-do list</h1>
       </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/"></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
