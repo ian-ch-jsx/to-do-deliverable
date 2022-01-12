1cb89ece0102a4560324c7dbf6a36e2c85dabf52
Ian Christiansen <ianc.songdog@gmail.com>
2022-01-11 17:35:08 -0800

    trying to fix site preview

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -5,11 +5,11 @@ export default function AuthForm({ email, setEmail, password, setPassword, error
       <div className="auth-container">
         <form className="form" onSubmit={handleSubmit}>
           <div className="form-controls">
-            <label>email</label>
+            <label>email: </label>
             <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
           </div>
           <div className="form-controls">
-            <label>password</label>
+            <label>password: </label>
             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
           </div>
           <input type="submit" className="submit-btn" />
